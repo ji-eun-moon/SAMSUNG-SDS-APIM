ca9591a0319eb13572bc58b589689e6ec4278582
chanwoong114 <cksdnd10001@gmail.com>
2023-10-23 16:27:21 +0900

    [BE] INIT: 프로젝트 생성

---
+++ itdaGateway/src/main/java/com/lego/itdagateway/ItdaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lego.itdagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItdaGatewayApplication.class, args);
+    }
+
+}
