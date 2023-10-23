209b17aee3b5f9f8f964e6030ce35cccb177a5db
문지은 <mjieun0956@gmail.com>
2023-10-23 12:03:09 +0900

    [FE] CHORE : SCSS 설치

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "src/styles/colors.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
