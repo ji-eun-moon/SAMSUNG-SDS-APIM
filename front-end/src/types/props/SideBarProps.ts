3f1653104a38ed8aaf80138449f1efb6d3b5de32
문지은 <mjieun0956@gmail.com>
2023-10-26 17:36:06 +0900

    [FE] FEAT : API SideBar 구현

--- front-end/src/types/props/SideBarProps.ts
+++ front-end/src/types/props/SideBarProps.ts
@@ -1,6 +1,30 @@
 import { TCategoryList } from '../Api';
 
-export interface SideBarProps {
+export interface ApiSideBarProps {
+  openCategory: number;
+  categoryList: TCategoryList;
+  my: false;
+}
+
+export interface MyApiSideBarProps {
+  openCategory: number;
+  useCategoryList: TCategoryList;
+  provideCategoryList: TCategoryList;
+  my: true;
+}
+
+export interface CategoryListProps {
   categoryList: TCategoryList;
   openCategory: number;
+}
+
+export interface IFilterCondition {
+  conditionId: string;
+  title: string;
+  url: string;
+}
+
+export interface SideBarMenuProps {
+  title: string;
+  conditionList: IFilterCondition[];
 }
