cac2ed15d9509f4039ff8cc4bb228296334684b0
이도하 <victoryddh5@naver.com>
2023-10-26 12:06:12 +0900

    FEAT : MemberService 회원등록 구현

--- memberService/src/main/java/com/itda/memberservice/member/service/MemberService.java
+++ memberService/src/main/java/com/itda/memberservice/member/service/MemberService.java
@@ -1,8 +1,41 @@
 package com.itda.memberservice.member.service;
 
 import com.itda.memberservice.member.dto.request.CreateMemberRequest;
-import com.itda.memberservice.member.dto.response.MemberResponse;
+import com.itda.memberservice.member.entity.Member;
+import com.itda.memberservice.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface MemberService {
-    MemberResponse createMember(CreateMemberRequest createMemberRequest);
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void register(List<CreateMemberRequest> requestList){
+
+        for (CreateMemberRequest request : requestList) {
+
+            if (memberRepository.existsMemberByEmployeeId(request.getEmployeeId())) {
+                continue;
+            }
+
+            Member save = memberRepository.save(Member.builder()
+                    .employeeId(request.getEmployeeId())
+                    .authority(request.getAuthority())
+                    .name(request.getName())
+                    .email(request.getEmail())
+                    .imageUrl(request.getImageUrl())
+                    .department(request.getDepartment())
+                    .position(request.getPosition())
+                    .build());
+
+            
+
+        }
+
+    }
+
 }
