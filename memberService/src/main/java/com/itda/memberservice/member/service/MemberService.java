333d5ab253a6a26d8c56196a3a9550ca7fa9e02a
이도하 <victoryddh5@naver.com>
2023-10-26 16:49:51 +0900

    [BE] FEAT : MemberService 회원등록 구현

--- memberService/src/main/java/com/itda/memberservice/member/service/MemberService.java
+++ memberService/src/main/java/com/itda/memberservice/member/service/MemberService.java
@@ -4,19 +4,27 @@ import com.itda.memberservice.member.dto.request.CreateMemberRequest;
 import com.itda.memberservice.member.entity.Member;
 import com.itda.memberservice.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder encoder;
 
     public Member register(CreateMemberRequest request){
 
         return memberRepository.save(Member.builder()
+                        .employeeId(request.getEmployeeId())
+                        .name(request.getName())
+                        .password(encoder.encode(request.getPassword()))
+                        .imageUrl(request.getImageUrl())
+                        .authority(request.getAuthority())
+                        .department(request.getDepartment())
+                        .position(request.getPosition())
+                        .email(request.getEmail())
                 .build());
 
     }
