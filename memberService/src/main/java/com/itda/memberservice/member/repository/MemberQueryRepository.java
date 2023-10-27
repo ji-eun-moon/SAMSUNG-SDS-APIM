bab5d2b72274eb02080d75069e0d5fbe36a43d32
이도하 <victoryddh5@naver.com>
2023-10-27 11:05:34 +0900

    [BE] FIX : 이름으로 검색 코드 수정

--- memberService/src/main/java/com/itda/memberservice/member/repository/MemberQueryRepository.java
+++ memberService/src/main/java/com/itda/memberservice/member/repository/MemberQueryRepository.java
@@ -1,11 +1,16 @@
 package com.itda.memberservice.member.repository;
 
+import com.itda.memberservice.member.dto.response.SearchMemberResponse;
 import com.itda.memberservice.member.entity.Member;
+
+import java.util.List;
 
 public interface MemberQueryRepository {
 
     void deleteByMemberId(Long memberId);
 
     Member findMemberByEmployeeId(String employeeId);
+
+    List<SearchMemberResponse> findByName(String name);
 
 }
