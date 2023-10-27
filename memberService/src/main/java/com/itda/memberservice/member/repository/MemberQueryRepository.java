4f790f7632bc51b53b5ce48bf717fead819fc3b0
이도하 <victoryddh5@naver.com>
2023-10-27 10:41:59 +0900

    [BE] FEAT : employeeId로 멤버 조회 쿼리 분리

--- memberService/src/main/java/com/itda/memberservice/member/repository/MemberQueryRepository.java
+++ memberService/src/main/java/com/itda/memberservice/member/repository/MemberQueryRepository.java
@@ -1,7 +1,11 @@
 package com.itda.memberservice.member.repository;
 
+import com.itda.memberservice.member.entity.Member;
+
 public interface MemberQueryRepository {
 
     void deleteByMemberId(Long memberId);
+
+    Member findMemberByEmployeeId(String employeeId);
 
 }
