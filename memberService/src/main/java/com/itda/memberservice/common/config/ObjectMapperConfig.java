cf7df801b240616506ac1cf1adec5895d1f8e21f
이도하 <victoryddh5@naver.com>
2023-10-27 09:29:29 +0900

    [BE] FEAT : Directory 파일 구조 변경

--- memberService/src/main/java/com/itda/memberservice/common/config/ObjectMapperConfig.java
+++ memberService/src/main/java/com/itda/memberservice/common/config/ObjectMapperConfig.java
@@ -1,4 +1,4 @@
-package com.itda.memberservice.config;
+package com.itda.memberservice.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
