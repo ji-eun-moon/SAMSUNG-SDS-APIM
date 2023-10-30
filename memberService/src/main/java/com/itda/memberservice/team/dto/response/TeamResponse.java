d12c8be4974a0bce492ac2a5db030c13baeda80b
이도하 <victoryddh5@naver.com>
2023-10-30 16:11:27 +0900

    [BE] FEAT : @ToString 추가

--- memberService/src/main/java/com/itda/memberservice/team/dto/response/TeamResponse.java
+++ memberService/src/main/java/com/itda/memberservice/team/dto/response/TeamResponse.java
@@ -3,10 +3,12 @@ package com.itda.memberservice.team.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class TeamResponse {
     private String teamName;
 }
