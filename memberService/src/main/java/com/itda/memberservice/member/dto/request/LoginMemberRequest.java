e92f0986706b4f77a87aebb603e7c58d5b0982e6
이도하 <victoryddh5@naver.com>
2023-10-30 15:02:20 +0900

    [BE] FEAT : @Builder 추가

--- memberService/src/main/java/com/itda/memberservice/member/dto/request/LoginMemberRequest.java
+++ memberService/src/main/java/com/itda/memberservice/member/dto/request/LoginMemberRequest.java
@@ -1,12 +1,14 @@
 package com.itda.memberservice.member.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class LoginMemberRequest {
 
     private String employeeId;
