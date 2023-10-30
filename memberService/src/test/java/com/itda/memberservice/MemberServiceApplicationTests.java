6ad1237f94a4bd3ab6fb918a7ace73a23b6e649f
이도하 <victoryddh5@naver.com>
2023-10-30 17:57:41 +0900

    [BE] TEST : contextLoads 삭제

--- memberService/src/test/java/com/itda/memberservice/MemberServiceApplicationTests.java
+++ memberService/src/test/java/com/itda/memberservice/MemberServiceApplicationTests.java
@@ -1,13 +1,9 @@
 package com.itda.memberservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MemberServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
