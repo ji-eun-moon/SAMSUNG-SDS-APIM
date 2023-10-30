f0a2e07f2b6d7e42bf78f1150b7fd1312187db45
이도하 <victoryddh5@naver.com>
2023-10-30 15:00:24 +0900

    [BE] FEAT : 로그인 성공 response 생성

---
+++ memberService/src/main/java/com/itda/memberservice/member/dto/response/LoginMemberResponse.java
@@ -0,0 +1,16 @@
+package com.itda.memberservice.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginMemberResponse {
+
+    private String token;
+
+}
