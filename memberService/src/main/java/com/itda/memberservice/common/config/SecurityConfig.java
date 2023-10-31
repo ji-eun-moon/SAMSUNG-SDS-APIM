edad6cd9ae0a6d9334ddc8b75fd27cf70777558c
이도하 <victoryddh5@naver.com>
2023-10-31 11:53:47 +0900

    [BE] FEAT : Security 주석 처리

--- memberService/src/main/java/com/itda/memberservice/common/config/SecurityConfig.java
+++ memberService/src/main/java/com/itda/memberservice/common/config/SecurityConfig.java
@@ -1,16 +1,10 @@
 package com.itda.memberservice.common.config;
 
-import com.itda.memberservice.common.security.JwtTokenFilter;
 import com.itda.memberservice.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -23,23 +17,23 @@ public class SecurityConfig {
     @Value("${security.jwt.secret.key}")
     private String secretKey;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
-
-        return security
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> configurationSource())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
-                        .requestMatchers("/**").permitAll())
-                .addFilterBefore(new JwtTokenFilter(memberService, secretKey), UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
-                        .requestMatchers("/swagger-ui/**", "/v3/**").permitAll())
-//                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
-                .build();
-
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+//
+//        return security
+//                .httpBasic(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .cors(cors -> configurationSource())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+//                        .requestMatchers("/**").permitAll())
+//                .addFilterBefore(new JwtTokenFilter(memberService, secretKey), UsernamePasswordAuthenticationFilter.class)
+//                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+//                        .requestMatchers("/swagger-ui/**", "/v3/**").permitAll())
+////                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+//                .build();
+//
+//    }
 
     @Bean
     public CorsConfigurationSource configurationSource(){
