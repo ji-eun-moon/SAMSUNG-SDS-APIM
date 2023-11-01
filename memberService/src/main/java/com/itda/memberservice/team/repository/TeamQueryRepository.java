1002ae1f2dcd062294aa97d535515a816ae8d338
이도하 <victoryddh5@naver.com>
2023-11-01 17:04:59 +0900

    [BE] FEAT : 팀 회원목록 조회

--- memberService/src/main/java/com/itda/memberservice/team/repository/TeamQueryRepository.java
+++ memberService/src/main/java/com/itda/memberservice/team/repository/TeamQueryRepository.java
@@ -1,4 +1,11 @@
 package com.itda.memberservice.team.repository;
 
+import com.itda.memberservice.member.dto.response.TeamMemberResponse;
+
+import java.util.List;
+
 public interface TeamQueryRepository {
+
+    List<TeamMemberResponse> findMembers(Long teamId);
+
 }
