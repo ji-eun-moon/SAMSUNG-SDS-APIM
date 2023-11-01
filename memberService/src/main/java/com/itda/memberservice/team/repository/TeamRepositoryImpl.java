c1ec3ad7bce660695af9a9066181cdc6d8571c43
이도하 <victoryddh5@naver.com>
2023-11-01 17:04:55 +0900

    [BE] FEAT : 팀 회원목록 조회

--- memberService/src/main/java/com/itda/memberservice/team/repository/TeamRepositoryImpl.java
+++ memberService/src/main/java/com/itda/memberservice/team/repository/TeamRepositoryImpl.java
@@ -1,15 +1,37 @@
 package com.itda.memberservice.team.repository;
 
+import com.itda.memberservice.member.dto.response.TeamMemberResponse;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 
+import java.util.List;
+
+import static com.itda.memberservice.member.entity.QMember.member;
+import static com.itda.memberservice.memberteam.entity.QMemberTeam.memberTeam;
+
 public class TeamRepositoryImpl implements TeamQueryRepository{
 
-    private final EntityManager em;
-    private final JPAQueryFactory jpaQueryFactory;
+    private final JPAQueryFactory queryFactory;
 
     public TeamRepositoryImpl(EntityManager em) {
-        this.em = em;
-        this.jpaQueryFactory = new JPAQueryFactory(em);
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public List<TeamMemberResponse> findMembers(Long teamId) {
+
+        return queryFactory
+                .select(Projections.fields(TeamMemberResponse.class,
+                        member.employeeId,
+                        member.name,
+                        member.imageUrl,
+                        member.email,
+                        member.department,
+                        member.position))
+                .from(memberTeam.member, member)
+                .where(memberTeam.team.teamId.eq(teamId))
+                .fetch();
+
     }
 }
