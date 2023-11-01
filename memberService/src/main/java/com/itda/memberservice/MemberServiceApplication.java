5651fd3920deb934115a3ff37ebc2d16148bb144
이도하 <victoryddh5@naver.com>
2023-11-01 15:35:40 +0900

    [BE] FEAT : @EnableJpaAuditing 추가

--- memberService/src/main/java/com/itda/memberservice/MemberServiceApplication.java
+++ memberService/src/main/java/com/itda/memberservice/MemberServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeReposi
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class MemberServiceApplication {
 
     public static void main(String[] args) {
