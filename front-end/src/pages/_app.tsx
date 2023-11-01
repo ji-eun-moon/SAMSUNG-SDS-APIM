f20d212d8a8898ecd60e1dc7b084ec613f38a18d
문지은 <mjieun0956@gmail.com>
2023-11-01 09:09:38 +0900

    [FE] FEAT : react-query 설정

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,19 +1,28 @@
+import { useState } from 'react';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 import { Noto_Sans_KR } from 'next/font/google';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
-// test2
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <NextUIProvider>
-      <main className={notoSansKr.className}>
-        <Component {...pageProps} />
-      </main>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <NextUIProvider>
+          <main className={notoSansKr.className}>
+            <Component {...pageProps} />
+          </main>
+        </NextUIProvider>
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
