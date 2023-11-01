4a69f5658bff6bda56bed7781720ca5e22703ebc
이도하 <victoryddh5@naver.com>
2023-11-01 16:32:01 +0900

    [BE] FEAT : @Builder 추가

--- memberService/src/main/java/com/itda/memberservice/member/dto/response/SearchMemberResponse.java
+++ memberService/src/main/java/com/itda/memberservice/member/dto/response/SearchMemberResponse.java
@@ -1,12 +1,14 @@
 package com.itda.memberservice.member.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SearchMemberResponse {
 
     private String employeeId;
@@ -14,4 +16,5 @@ public class SearchMemberResponse {
     private String department;
     private String position;
     private String imageUrl;
+
 }
