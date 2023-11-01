75d472aa0f61dbdde3019f5f1049b65379f4a99b
송아람 <0807com@naver.com>
2023-11-02 06:52:04 +0900

    [FE] FEAT : api status

--- front-end/src/pages/apis/status.tsx
+++ front-end/src/pages/apis/status.tsx
@@ -1,3 +1,30 @@
+// import { IUser } from '@/types/User';
+// import { NextPage, GetServerSideProps } from 'next';
+// import { getApiStatus } from '@/utils/axios/api';
+// import { useQuery, QueryClient } from 'react-query';
+// import { dehydrate } from 'react-query/hydration';
+
+// const ApiStatus: NextPage = () => {
+//   const { data: userInfo } = useQuery<IUser>('apiStatus', getApiStatus);
+
+//   if (userInfo === undefined) {
+//     return null;
+//   }
+//   return <div>API 상태페이지</div>;
+// };
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery('apiStatus', getApiStatus);
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
+
+// export default ApiStatus;
+
 export default function ApiStatus() {
   return <div>API 상태 페이지</div>;
 }
--- front-end/src/utils/axios/api.ts
+++ front-end/src/utils/axios/api.ts
@@ -27,3 +27,20 @@ export async function getGetegoryListTest() {
     return null;
   }
 }
+
+export async function getApiStatus(props: string) {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: 'http://localhost:3001/categoryList',
+      params: {
+        props,
+      },
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
