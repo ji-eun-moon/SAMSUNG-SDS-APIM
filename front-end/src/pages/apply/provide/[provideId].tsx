040f8e8165507e175ccf98abe5efe0a1d1d36c78
박서희 <dudntjgml@gmail.com>
2023-11-02 21:11:43 +0900

    [FE] FEAT : 제공신청 내역 상세 페이지 구현

--- front-end/src/pages/apply/provide/[provideId].tsx
+++ front-end/src/pages/apply/provide/[provideId].tsx
@@ -1,220 +1,3 @@
-// // import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-// // import { IProvide, IProvideDetail } from '@/types/Apply';
-// // import { getProvideApplyDetail, getProvideApplyId } from '@/utils/axios/apply';
-// // import ApplySideBar from '@/components/organisms/ApplySideBar';
-// // import BothLayout from '@/components/templates/BothLayout';
-// // import GoBack from '@/components/atoms/GoBack';
-// // import RowTable from '@/components/atoms/RowTable';
-// // import style from '@/styles/ProvideDetail.module.scss';
-// // import { QueryClient } from 'react-query';
-
-// // interface IProvideId extends IProvide, IProvideDetail {}
-
-// // type SSGProps = {
-// //   details: IProvideId[];
-// //   isUser: boolean;
-// // };
-
-// // const ProvideDetail: NextPage<SSGProps> = ({ details, isUser }: SSGProps) => {
-// //   const headerContent = ['서버명', '서버설명', '신청팀', '담당자', '서버주소'];
-// //   const bodyContent = details?.map((detail) => ({
-// //     서버명: detail.serverName,
-// //     서버설명: detail.description,
-// //     신청팀: detail.teamName,
-// //     담당자: detail.providerName,
-// //     서버주소: detail.apiDocs,
-// //   }));
-
-// //   return (
-// //     <BothLayout>
-// //       <div>Top Nav 들어갈 부분</div>
-// //       <ApplySideBar isUser={isUser} />
-// //       <div className={`${style.listContainer}`}>
-// //         <GoBack label="API 제공 신청 내역" />
-// //         <RowTable title="신청 정보" headerContent={headerContent} bodyContent={bodyContent} />
-// //       </div>
-// //     </BothLayout>
-// //   );
-// // };
-// // export default ProvideDetail;
-
-// // // export const getStaticPaths: GetStaticPaths = async () => {
-// // //   // const ids = await getProvideApplyId();
-// // //   // console.log('ids', ids);
-// // //   // const paths =
-// // //   //   ids?.map((id: IProvideId) => ({
-// // //   //     params: { provideId: id.provideId.toString() },
-// // //   //   })) || [];
-// // //   const paths = [{ params: { provideId: '1' } }];
-// // //   console.log(paths);
-// // //   return {
-// // //     paths,
-// // //     fallback: true, // fallback을 true로 설정하면, 존재하지 않는 경로로 접근 시 404 페이지가 아닌 서버사이드 렌더링이 이루어집니다.
-// // //   };
-// // // };
-
-// // // export const getStaticProps: GetStaticProps = async ({ params }) => {
-// // //   const queryClient = new QueryClient();
-// // //   const provideId = params?.provideId; // 경로 매개변수에서 provideId 가져오기
-
-// // //   if (!provideId) {
-// // //     return {
-// // //       notFound: true,
-// // //     };
-// // //   }
-
-// // //   const details = await queryClient.prefetchQuery('provideApplyDetail', () => getProvideApplyDetail(Number(provideId)));
-// // //   const isUser = true;
-// // //   console.log('details', details);
-
-// // //   if (details === undefined) {
-// // //     return {
-// // //       props: {
-// // //         details: null,
-// // //         isUser: true,
-// // //       },
-// // //       revalidate: 60,
-// // //     };
-// // //   }
-
-// // //   return {
-// // //     props: {
-// // //       details,
-// // //       isUser,
-// // //     },
-// // //     revalidate: 60,
-// // //   };
-// // // };
-// // // export default ProvideDetail;
-
-// import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-// import { IProvide, IProvideDetail } from '@/types/Apply';
-// import { getProvideApplyDetail } from '@/utils/axios/apply';
-// import ApplySideBar from '@/components/organisms/ApplySideBar';
-// import BothLayout from '@/components/templates/BothLayout';
-// import GoBack from '@/components/atoms/GoBack';
-// import RowTable from '@/components/atoms/RowTable';
-// import style from '@/styles/ProvideDetail.module.scss';
-
-// import React, { useEffect, useState } from 'react';
-
-// interface IProvideId extends IProvide, IProvideDetail {}
-
-// function ProvideDetail() {
-//   const [detail, setDetail] = useState<IProvideId | null>(null);
-//   const [bodyContent, setBodyContent] = useState({});
-//   const isUser = true;
-
-//   useEffect(() => {
-//     console.log('야야야아아아ㅏㅇ');
-//     const fetchData = async () => {
-//       const data = await getProvideApplyDetail(1);
-//       setDetail(data);
-//       const hi = [
-//         {
-//           서버명: data.serverName,
-//           서버설명: data.description,
-//           신청팀: data.teamName,
-//           담당자: data.providerName,
-//           서버주소: data.apiDocs,
-//         },
-//       ];
-//       setBodyContent(hi);
-//     };
-//     fetchData();
-//   }, []);
-//   console.log('제에에에에에에에에에에에에엥에ㅔ엥에에에에발', detail);
-//   const headerContent = ['서버명', '서버설명', '신청팀', '담당자', '서버주소'];
-//   // const bodyContent = [
-//   //   {
-//   //     서버명: detail.serverName,
-//   //     서버설명: detail.description,
-//   //     신청팀: detail.teamName,
-//   //     담당자: detail.providerName,
-//   //     서버주소: detail.apiDocs,
-//   //   },
-//   // ];
-
-//   return (
-//     <BothLayout>
-//       <div>Top Nav 들어갈 부분</div>
-//       <ApplySideBar isUser={isUser} />
-//       <div className={`${style.listContainer}`}>
-//         <GoBack label="API 제공 신청 내역" />
-//         <RowTable title="신청 정보" headerContent={headerContent} bodyContent={bodyContent} />
-//       </div>
-//     </BothLayout>
-//   );
-// }
-
-// export default ProvideDetail;
-// //
-// // interface IProvideId extends IProvide, IProvideDetail {}
-
-// // type SSGProps = {
-// //   detail: IProvideId; // 단일 데이터로 변경
-// //   isUser: boolean;
-// // };
-
-// // const ProvideDetail: NextPage<SSGProps> = ({ detail, isUser }: SSGProps) => {
-// //   const headerContent = ['서버명', '서버설명', '신청팀', '담당자', '서버주소'];
-// //   const bodyContent = [
-// //     {
-// //       서버명: detail.serverName,
-// //       서버설명: detail.description,
-// //       신청팀: detail.teamName,
-// //       담당자: detail.providerName,
-// //       서버주소: detail.apiDocs,
-// //     },
-// //   ];
-
-// //   return (
-// //     <BothLayout>
-// //       <div>Top Nav 들어갈 부분</div>
-// //       <ApplySideBar isUser={isUser} />
-// //       <div className={`${style.listContainer}`}>
-// //         <GoBack label="API 제공 신청 내역" />
-// //         <RowTable title="신청 정보" headerContent={headerContent} bodyContent={bodyContent} />
-// //       </div>
-// //     </BothLayout>
-// //   );
-// // };
-
-// // export default ProvideDetail;
-
-// // export const getStaticPaths: GetStaticPaths = async () => {
-// //   // 여기서 가능한 provideId 목록을 가져와야 합니다.
-// //   // 예를 들어 [1, 2, 3]이 가능하다고 가정합니다.
-// //   const possibleProvideIds = [1, 2, 3];
-
-// //   const paths = possibleProvideIds.map((provideId) => ({
-// //     params: { provideId: provideId.toString() },
-// //   }));
-
-// //   return { paths, fallback: false };
-// // };
-
-// // export const getStaticProps: GetStaticProps<SSGProps> = async ({ params }) => {
-// //   const provideId = params?.provideId as string;
-// //   const parsedProvideId = parseInt(provideId, 10);
-
-// //   if (Number.isNaN(parsedProvideId)) {
-// //     return {
-// //       notFound: true,
-// //     };
-// //   }
-
-// //   const detail = await getProvideApplyDetail(parsedProvideId);
-
-// //   return {
-// //     props: {
-// //       detail,
-// //       isUser: true, // 필요에 따라 수정하세요.
-// //     },
-// //     revalidate: 60, // 60초마다 페이지를 재생성합니다. 필요에 따라 조절하세요.
-// //   };
-// // };
-
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { IProvideDetail } from '@/types/Apply';
 import { getProvideApplyDetail } from '@/utils/axios/apply';
@@ -240,10 +23,20 @@ const ProvideDetail: NextPage<SSGProps> = ({ isUser, provideId }: SSGProps) => {
     return null;
   }
 
-  const headerContent = ['서버명', '서버설명', '신청팀', '담당자', '서버주소'];
-  const bodyContent = [
+  const onApplyType = (applyType: string) => {
+    if (applyType === '신규') {
+      return `newnew${details.serverName}`;
+    }
+    if (applyType === '변경') {
+      return `chacha${details.serverName}`;
+    }
+    return `${details.serverName}`;
+  };
+
+  const headerContentT = ['서버명', '서버설명', '신청팀', '담당자', '서버주소'];
+  const bodyContentT = [
     {
-      서버명: details.serverName,
+      서버명: onApplyType(details.applyType),
       서버설명: details.description,
       신청팀: details.teamName,
       담당자: details.providerName,
@@ -251,13 +44,39 @@ const ProvideDetail: NextPage<SSGProps> = ({ isUser, provideId }: SSGProps) => {
     },
   ];
 
+  const headerContentB = ['처리상태', '처리내용'];
+  let 처리내용 = '';
+
+  if (details.state === '승인') {
+    처리내용 = `${details.serverName} 서버 신청 승인되었습니다`;
+  } else if (details.state === '거절') {
+    처리내용 = details.denyReason;
+  } else {
+    처리내용 = `${details.serverName} 서버 신청 대기중입니다.`;
+  }
+
+  const bodyContentB = [
+    {
+      처리상태: details.state,
+      처리내용,
+    },
+  ];
+
   return (
     <BothLayout>
-      <div>Top Nav 들어갈 부분</div>
       <ApplySideBar isUser={isUser} />
-      <div className={`${style.listContainer}`}>
-        <GoBack label="API 제공 신청 내역" />
-        <RowTable title="신청 정보" headerContent={headerContent} bodyContent={bodyContent} />
+      <div className={`${style.detailContainer}`}>
+        <div className={`${style.label}`}>
+          <GoBack label="서버 제공 신청 내역" />
+        </div>
+        <div className={`${style.tableContainer}`}>
+          <div className={`${style.table}`}>
+            <RowTable title="신청 정보" headerContent={headerContentT} bodyContent={bodyContentT} />
+          </div>
+          <div className={`${style.table}`}>
+            <RowTable title="신청 상태" headerContent={headerContentB} bodyContent={bodyContentB} />
+          </div>
+        </div>
       </div>
     </BothLayout>
   );
