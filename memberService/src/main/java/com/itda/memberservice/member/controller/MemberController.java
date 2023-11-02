07d336f09a17faf00a57a490e8afa912253c4b44
이도하 <victoryddh5@naver.com>
2023-11-02 10:01:28 +0900

    [BE] FEAT : 회원 로그인시 에러 상태 코드 변경

--- memberService/src/main/java/com/itda/memberservice/member/controller/MemberController.java
+++ memberService/src/main/java/com/itda/memberservice/member/controller/MemberController.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -79,7 +78,7 @@ public class MemberController {
             @ApiResponse(responseCode = "404", description = "Not Found"),
             @ApiResponse(responseCode = "500", description = "Server Error")
     })
-    public ResponseEntity<?> logina(@RequestBody LoginMemberRequest request, HttpServletResponse response) {
+    public ResponseEntity<?> login(@RequestBody LoginMemberRequest request) {
 
         try {
 
