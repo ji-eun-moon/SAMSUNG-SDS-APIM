9479492653e503e484ab0498d0eb335f5e961541
문지은 <mjieun0956@gmail.com>
2023-11-02 15:23:40 +0900

    [FE] FEAT : 카테고리 없을 때 바로가기 오류 수정

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -6,34 +6,24 @@ import ApplySummary from '@/components/organisms/ApplySummary';
 import StatusSummary from '@/components/organisms/StatusSummary';
 import { NextPage, GetServerSideProps } from 'next';
 import { getUserInfo } from '@/utils/axios/user';
-import { QueryClient, useQuery } from 'react-query';
+import { QueryClient } from 'react-query';
 import { dehydrate } from 'react-query/hydration';
 import { getCategoryList } from '@/utils/axios/api';
-import { TCategoryList } from '@/types/Api';
 
-const Home: NextPage = () => {
-  const { data: categoryList } = useQuery<TCategoryList>('categoryList', getCategoryList);
-
-  let firstCategory = 0;
-  if (categoryList) {
-    firstCategory = categoryList[0]?.categoryId || 0;
-  }
-
-  return (
-    <main>
-      <SideLayout>
-        <div className={`${style.pageContainer}`}>
-          {/* 바로가기 탭 */}
-          <ShortCuts firstCategory={firstCategory} />
-          {/* API 신청 내역 */}
-          <ApplySummary />
-          {/* API 상태 */}
-          <StatusSummary />
-        </div>
-      </SideLayout>
-    </main>
-  );
-};
+const Home: NextPage = () => (
+  <main>
+    <SideLayout>
+      <div className={`${style.pageContainer}`}>
+        {/* 바로가기 탭 */}
+        <ShortCuts />
+        {/* API 신청 내역 */}
+        <ApplySummary />
+        {/* API 상태 */}
+        <StatusSummary />
+      </div>
+    </SideLayout>
+  </main>
+);
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const queryClient = new QueryClient();
