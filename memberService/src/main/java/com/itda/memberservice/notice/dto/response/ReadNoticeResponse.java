9c830be8064ebf119b04b363abfb609e998b84bc
이도하 <victoryddh5@naver.com>
2023-11-02 11:52:03 +0900

    [BE] FEAT : 읽은 메시지 dto 생성

---
+++ memberService/src/main/java/com/itda/memberservice/notice/dto/response/ReadNoticeResponse.java
@@ -0,0 +1,23 @@
+package com.itda.memberservice.notice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReadNoticeResponse {
+
+    private Long noticeId;
+    private Long memberId;
+    private String senderName;
+    private String senderImage;
+    private String title;
+    private LocalDateTime createdAt;
+
+}
