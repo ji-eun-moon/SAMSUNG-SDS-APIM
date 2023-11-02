12876d94683065feeaf4d9fe56fe8a936ecdbf82
victoryddh <victoryddh5@naver.com>
2023-11-02 22:14:41 +0900

    [BE] TEST : 테스트컨트롤러 주석 처리

--- memberService/src/test/java/com/itda/memberservice/common/ControllerTest.java
+++ memberService/src/test/java/com/itda/memberservice/common/ControllerTest.java
@@ -1,31 +1,31 @@
-package com.itda.memberservice.common;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.itda.memberservice.member.controller.MemberController;
-import com.itda.memberservice.member.service.MemberService;
-import com.itda.memberservice.memberteam.service.MemberTeamService;
-import com.itda.memberservice.notice.service.NoticeService;
-import com.itda.memberservice.team.service.TeamService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-
-@AutoConfigureRestDocs
-@WebMvcTest(
-        MemberController.class
-)
-@ActiveProfiles("test")
-public abstract class ControllerTest {
-
-    @Autowired protected MockMvc mvc;
-    @Autowired protected ObjectMapper objectMapper;
-
-    @MockBean protected MemberService memberService;
-    @MockBean protected MemberTeamService memberTeamService;
-    @MockBean protected TeamService teamService;
-    @MockBean protected NoticeService noticeService;
-
-}
+//package com.itda.memberservice.common;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.itda.memberservice.member.controller.MemberController;
+//import com.itda.memberservice.member.service.MemberService;
+//import com.itda.memberservice.memberteam.service.MemberTeamService;
+//import com.itda.memberservice.notice.service.NoticeService;
+//import com.itda.memberservice.team.service.TeamService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@AutoConfigureRestDocs
+//@WebMvcTest(
+//        MemberController.class
+//)
+//@ActiveProfiles("test")
+//public abstract class ControllerTest {
+//
+//    @Autowired protected MockMvc mvc;
+//    @Autowired protected ObjectMapper objectMapper;
+//
+//    @MockBean protected MemberService memberService;
+//    @MockBean protected MemberTeamService memberTeamService;
+//    @MockBean protected TeamService teamService;
+//    @MockBean protected NoticeService noticeService;
+//
+//}
