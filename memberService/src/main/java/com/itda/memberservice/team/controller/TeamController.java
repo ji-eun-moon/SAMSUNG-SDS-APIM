7ab73f56fee46fee9c5efc0c70a74d77293191f0
이도하 <victoryddh5@naver.com>
2023-11-03 10:47:52 +0900

    [BE] FEAT : Swagger 추가

--- memberService/src/main/java/com/itda/memberservice/team/controller/TeamController.java
+++ memberService/src/main/java/com/itda/memberservice/team/controller/TeamController.java
@@ -1,6 +1,7 @@
 package com.itda.memberservice.team.controller;
 
 import com.itda.memberservice.team.service.TeamService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class TeamController {
     private final TeamService teamService;
 
     @GetMapping("/{teamName}")
+    @Operation(summary = "팀 회원 조회", description = "해당 팀 이름에 해당하는 회원들 목록 조회")
     public ResponseEntity<?> getTeamMembers(@PathVariable("teamName") String teamName) {
 
         return ResponseEntity.ok(teamService.findMembers(teamName));
