ca070e13817676fa13be52ac5c1e970efbb07484
이도하 <victoryddh5@naver.com>
2023-11-03 15:38:43 +0900

    [BE] FEAT : 팀 정보에 토큰 추가

--- memberService/src/main/java/com/itda/memberservice/team/service/TeamService.java
+++ memberService/src/main/java/com/itda/memberservice/team/service/TeamService.java
@@ -1,14 +1,20 @@
 package com.itda.memberservice.team.service;
 
 
+import com.itda.memberservice.client.useCheck.UseCheckClient;
+import com.itda.memberservice.client.useCheck.dto.CategoryTokenResponse;
 import com.itda.memberservice.member.dto.response.TeamMemberResponse;
+import com.itda.memberservice.team.dto.response.TeamListResponse;
 import com.itda.memberservice.team.entity.Team;
 import com.itda.memberservice.team.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -17,6 +23,7 @@ import java.util.Optional;
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    private final UseCheckClient useCheckClient;
 
     public Team registerTeamOrFind(String teamName){
 
@@ -28,11 +35,27 @@ public class TeamService {
 
     }
 
-    public List<TeamMemberResponse> findMembers(String teamName){
+    public TeamListResponse getTeamInformation(String teamName){
 
-        log.info("{TeamService-findMembers} : teamName = " + teamName);
+        log.info("{TeamService-getTeamInformation} : teamName = " + teamName);
 
-        return teamRepository.findMembers(teamName);
+        Map<String, String> map = new HashMap<>();
+        map.put("teamName", teamName);
+
+        List<CategoryTokenResponse> tokens = useCheckClient.getToken(map);
+
+        List<TeamMemberResponse> members = teamRepository.findMembers(teamName);
+
+        Team team = teamRepository.findByName(teamName)
+                .orElseThrow(() -> new NotFoundException("해당하는 팀은 존재하지 않습니다."));
+
+        return TeamListResponse.builder()
+                .teamMembers(members)
+                .teamCount(members.size())
+                .teamId(team.getTeamId())
+                .teamName(team.getName())
+                .tokenResponses(tokens)
+                .build();
 
     }
 
