33fbd633db36ab5de4e941608b7ac6746757b128
이도하 <victoryddh5@naver.com>
2023-11-03 15:39:19 +0900

    [BE] FEAT : 팀 정보에 토큰 추가

--- memberService/src/main/java/com/itda/memberservice/team/controller/TeamController.java
+++ memberService/src/main/java/com/itda/memberservice/team/controller/TeamController.java
@@ -3,25 +3,27 @@ package com.itda.memberservice.team.controller;
 import com.itda.memberservice.team.service.TeamService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/team")
+@Slf4j
 public class TeamController {
 
     private final TeamService teamService;
 
-    @GetMapping("/{teamName}")
-    @Operation(summary = "팀 회원 조회", description = "해당 팀 이름에 해당하는 회원들 목록 조회")
-    public ResponseEntity<?> getTeamMembers(@PathVariable("teamName") String teamName) {
+    @GetMapping("")
+    @Operation(summary = "팀 회원 및 토큰 조회", description = "해당 팀 이름에 해당하는 회원들 목록과 사용중인 토큰 조회")
+    public ResponseEntity<?> getTeamInformation(@RequestParam("teamName") String teamName) {
 
-        return ResponseEntity.ok(teamService.findMembers(teamName));
+        log.info("{TeamController} : getTeamInformation \n" +
+                "teamName = " + teamName);
+
+        return ResponseEntity.ok(teamService.getTeamInformation(teamName));
 
     }
 
