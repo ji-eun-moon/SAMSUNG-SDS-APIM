8629287a8dd48dfdb91c0f29b16465a08bd4db21
이도하 <victoryddh5@naver.com>
2023-11-03 15:39:47 +0900

    [BE] RENAME : 파일 구조 변경

--- memberService/src/main/java/com/itda/memberservice/memberteam/service/MemberTeamService.java
+++ memberService/src/main/java/com/itda/memberservice/memberteam/service/MemberTeamService.java
@@ -1,10 +1,9 @@
-package com.itda.memberservice.memberTeam.service;
+package com.itda.memberservice.memberteam.service;
 
 import com.itda.memberservice.member.entity.Member;
-import com.itda.memberservice.memberTeam.repository.MemberTeamRepository;
 import com.itda.memberservice.memberteam.dto.request.MemberTeamCheckRequest;
 import com.itda.memberservice.memberteam.entity.MemberTeam;
-
+import com.itda.memberservice.memberteam.repository.MemberTeamRepository;
 import com.itda.memberservice.team.entity.Team;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
