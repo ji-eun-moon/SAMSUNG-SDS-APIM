e2b93256df2d626b37a96960c8da8b85c0675c8f
이도하 <victoryddh5@naver.com>
2023-11-03 15:39:12 +0900

    [BE] FEAT : 팀 정보에 토큰 추가

--- memberService/src/main/java/com/itda/memberservice/team/dto/response/TeamListResponse.java
+++ memberService/src/main/java/com/itda/memberservice/team/dto/response/TeamListResponse.java
@@ -1,7 +1,9 @@
 package com.itda.memberservice.team.dto.response;
 
+import com.itda.memberservice.client.useCheck.dto.CategoryTokenResponse;
 import com.itda.memberservice.member.dto.response.TeamMemberResponse;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,10 +12,13 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TeamListResponse {
 
      private Long teamId;
      private String teamName;
      private Integer teamCount;
      private List<TeamMemberResponse> teamMembers;
+     private List<CategoryTokenResponse> tokenResponses;
+
 }
