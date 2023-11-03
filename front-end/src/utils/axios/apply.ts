8327f47598f582cfe72092d0966cf8363efc900f
ptjgml <dudntjgml@gmail.com>
2023-11-03 17:45:04 +0900

    [FE] FEAT : 제공신청내역 사이드바에 따른 데이터 패칭

--- front-end/src/utils/axios/apply.ts
+++ front-end/src/utils/axios/apply.ts
@@ -1,6 +1,6 @@
 import axiosInstance from './axiosInstance';
 
-// 사용신청 내역
+// 사용신청 내역(사원)
 export async function getUseApplyList() {
   console.log('hi');
   try {
@@ -21,8 +21,8 @@ export async function getUseApplyList() {
   }
 }
 
-// 제공신청 내역
-export async function getProvideApplyList(clickPage: number) {
+// 제공신청 내역(사원)
+export async function getProvideApplyList(clickPage: number, state: string) {
   console.log('provide');
   try {
     const response = await axiosInstance({
@@ -30,6 +30,7 @@ export async function getProvideApplyList(clickPage: number) {
       url: '/submit/provide/team',
       params: {
         teamName: 'project4',
+        state,
         page: clickPage,
         size: 10,
       },
@@ -42,7 +43,7 @@ export async function getProvideApplyList(clickPage: number) {
   }
 }
 
-// 제공 신청 내역 상세
+// 제공 신청 내역 상세(사원)
 export async function getProvideApplyDetail(provideId: number) {
   try {
     const response = await axiosInstance({
@@ -50,6 +51,28 @@ export async function getProvideApplyDetail(provideId: number) {
       url: `/submit/provide/${provideId}`,
     });
     console.log('제공신청내역상세', response.data.content);
+    return response.data;
+  } catch (error) {
+    console.error('error', error);
+    return null;
+  }
+}
+
+
+// 제공신청 내역(관리자)
+export async function getAdminProvideApplyList(clickPage: number, state: string) {
+  console.log('provide');
+  try {
+    const response = await axiosInstance({
+      method: 'GET',
+      url: '/submit/provide',
+      params: {
+        state,
+        page: clickPage,
+        size: 10,
+      },
+    });
+    console.log('제공신청내역(관리자)', response.data);
     return response.data;
   } catch (error) {
     console.error('error', error);
