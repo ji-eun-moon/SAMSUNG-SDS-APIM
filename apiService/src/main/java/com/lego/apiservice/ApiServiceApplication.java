f3cfde985f0aefbe11a6fc49e0e94c0baa94a854
chanwoong114 <cksdnd10001@gmail.com>
2023-11-07 00:12:04 +0900

    [BE] FEAT: 시간 설정

--- apiService/src/main/java/com/lego/apiservice/ApiServiceApplication.java
+++ apiService/src/main/java/com/lego/apiservice/ApiServiceApplication.java
@@ -1,14 +1,22 @@
 package com.lego.apiservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
 public class ApiServiceApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiServiceApplication.class, args);
