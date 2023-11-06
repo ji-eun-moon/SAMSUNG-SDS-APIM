01798b8e431b8a0dde38341c1d4f9df1c2bc2c1e
문지은 <mjieun0956@gmail.com>
2023-11-06 16:52:34 +0900

    [FE] FEAT : NavBar Notice 구현

---
+++ front-end/src/utils/dropDown.tsx
@@ -0,0 +1,194 @@
+import { IDropdownItem } from '../types/props/NavBarProps';
+
+interface IParams {
+  categoryId: number;
+  apiId: number;
+}
+
+export const getUserDropDownList = ({ categoryId, apiId }: IParams): IDropdownItem[] => {
+  const userDropDown: IDropdownItem[] = [
+    {
+      title: 'API 상태',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+        </svg>
+      ),
+      url: `/apis/status`,
+    },
+    {
+      title: '통계',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-600 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 18"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 12v5m5-9v9m5-5v5m5-9v9M1 7l5-6 5 6 5-6"
+          />
+        </svg>
+      ),
+      url: `/statistics/${apiId}`,
+    },
+    {
+      title: '서버 모니터링',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-600 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 18"
+        >
+          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-7.5 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2Zm-3 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2Zm-3 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2ZM2 16V8h16v8H2Z" />
+        </svg>
+      ),
+      url: '/monitoring',
+    },
+    {
+      title: '신청 내역',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-600 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="20"
+          fill="none"
+          viewBox="0 0 18 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeWidth="2"
+            d="M12 2h4a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4m6 0v3H6V2m6 0a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1M5 5h8m-5 5h5m-8 0h.01M5 14h.01M8 14h5"
+          />
+        </svg>
+      ),
+      url: '/apply/use/list',
+    },
+    {
+      title: 'API 목록',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 22"
+        >
+          <path d="M15.458 4a3 3 0 1 0-4.478 2.6A2.6 2.6 0 0 1 8.4 9H6.6a4.57 4.57 0 0 0-2.6.814v-3a3 3 0 1 0-2 0v8.368a3 3 0 1 0 2 0V13.6A2.607 2.607 0 0 1 6.6 11h1.8a4.6 4.6 0 0 0 4.548-4.049A3 3 0 0 0 15.458 4Z" />
+        </svg>
+      ),
+      url: `/category/${categoryId}`,
+    },
+  ];
+  return userDropDown;
+};
+
+export const getAdminDropDownList = ({ categoryId, apiId }: IParams): IDropdownItem[] => {
+  const adminDropDown: IDropdownItem[] = [
+    {
+      title: 'API 상태',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+        </svg>
+      ),
+      url: `/apis/status`,
+    },
+    {
+      title: '통계',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-600 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 18"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 12v5m5-9v9m5-5v5m5-9v9M1 7l5-6 5 6 5-6"
+          />
+        </svg>
+      ),
+      url: `/statistics/${apiId}`,
+    },
+    {
+      title: '서버 모니터링',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-600 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 18"
+        >
+          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-7.5 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2Zm-3 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2Zm-3 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2ZM2 16V8h16v8H2Z" />
+        </svg>
+      ),
+      url: '/monitoring',
+    },
+    {
+      title: '신청 내역',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-600 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="20"
+          fill="none"
+          viewBox="0 0 18 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeWidth="2"
+            d="M12 2h4a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4m6 0v3H6V2m6 0a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1M5 5h8m-5 5h5m-8 0h.01M5 14h.01M8 14h5"
+          />
+        </svg>
+      ),
+      url: '/admin/useApplyList',
+    },
+    {
+      title: 'API 목록',
+      icon: (
+        <svg
+          className="w-4 h-4 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 22"
+        >
+          <path d="M15.458 4a3 3 0 1 0-4.478 2.6A2.6 2.6 0 0 1 8.4 9H6.6a4.57 4.57 0 0 0-2.6.814v-3a3 3 0 1 0-2 0v8.368a3 3 0 1 0 2 0V13.6A2.607 2.607 0 0 1 6.6 11h1.8a4.6 4.6 0 0 0 4.548-4.049A3 3 0 0 0 15.458 4Z" />
+        </svg>
+      ),
+      url: `/category/${categoryId}`,
+    },
+  ];
+  return adminDropDown;
+};
