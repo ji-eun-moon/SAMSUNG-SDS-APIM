507975c92c247f6c1bff20c5b3d95a30325d5ad7
chanwoong114 <cksdnd10001@gmail.com>
2023-11-06 11:18:25 +0900

    [BE] FEAT: API 사용량 mongodb 연결

--- apiService/src/main/java/com/lego/apiservice/api/service/ApiBatchService.java
+++ apiService/src/main/java/com/lego/apiservice/api/service/ApiBatchService.java
@@ -1,4 +1,14 @@
 package com.lego.apiservice.api.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class ApiBatchService {
+
+//    @Scheduled(cron = "")
 }
