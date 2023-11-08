83bfdad16e0a9008a9ce746428f831e521f9b752
이도하 <victoryddh5@naver.com>
2023-11-08 16:47:21 +0900

    [BE] FEAT : 결과 쪽지 전송

--- memberService/src/main/java/com/itda/memberservice/memberteam/repository/MemberTeamQueryRepository.java
+++ memberService/src/main/java/com/itda/memberservice/memberteam/repository/MemberTeamQueryRepository.java
@@ -1,7 +1,13 @@
 package com.itda.memberservice.memberteam.repository;
 
+import com.itda.memberservice.member.entity.Member;
+
+import java.util.List;
+
 public interface MemberTeamQueryRepository {
 
     Boolean check(String employeeId, String teamName);
+
+    List<Member> findMembersByTeam(String teamName);
 
 }
