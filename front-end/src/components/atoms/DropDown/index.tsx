20f34813cbc3079751b94d8851639490c4974ea9
송아람 <0807com@naver.com>
2023-11-08 23:16:34 +0900

    [FE] FEAT : notice 수정

--- front-end/src/components/atoms/DropDown/index.tsx
+++ front-end/src/components/atoms/DropDown/index.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from '@nextui-org/react';
-import { IDropdownItem } from '@/types/props/NavBarProps';
+import { DropDownProps, UrlProps, ModalProps } from '@/types/props/DropDownProps';
 import { useRouter } from 'next/router';
 
-interface DropDownProps {
-  list: IDropdownItem[];
-  trigger: React.ReactNode;
-}
-
-function DropDown({ list, trigger }: DropDownProps) {
+function DropDown({ type, ...props }: DropDownProps) {
   const router = useRouter();
-  return (
-    <Dropdown>
-      <DropdownTrigger>{trigger}</DropdownTrigger>
-      <DropdownMenu variant="flat">
-        {list?.map((item) => (
-          <DropdownItem key={item.title} onClick={() => router.push(`${item.url}`)} startContent={item.icon}>
-            {item.title}
-          </DropdownItem>
-        ))}
-      </DropdownMenu>
-    </Dropdown>
-  );
+
+  if (type === 'url') {
+    const { trigger, list } = props as UrlProps;
+    return (
+      <Dropdown>
+        <DropdownTrigger>{trigger}</DropdownTrigger>
+        <DropdownMenu variant="flat">
+          {list?.map((item) => (
+            <DropdownItem key={item.title} onClick={() => router.push(`${item.url}`)} startContent={item.icon}>
+              {item.title}
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
+
+  if (type === 'modal') {
+    const {trigger, list} = props as ModalProps;
+    return (
+      <Dropdown>
+        <DropdownTrigger>{trigger}</DropdownTrigger>
+        <DropdownMenu variant="flat">
+          {list?.map((item) => (
+            <DropdownItem key={item.title} onClick={item.onModalHandler} startContent={item.icon}>
+              {item.title}
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
 }
 
 export default DropDown;
