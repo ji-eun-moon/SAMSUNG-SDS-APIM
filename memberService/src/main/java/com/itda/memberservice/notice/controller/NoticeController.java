46d6283413760086e9795c2c58945d0e3b342b6f
이도하 <victoryddh5@naver.com>
2023-11-08 16:12:31 +0900

    [BE] FEAT : unused Import 제거

--- memberService/src/main/java/com/itda/memberservice/notice/controller/NoticeController.java
+++ memberService/src/main/java/com/itda/memberservice/notice/controller/NoticeController.java
@@ -11,9 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RestController
 @RequestMapping("/notice")
 @RequiredArgsConstructor
