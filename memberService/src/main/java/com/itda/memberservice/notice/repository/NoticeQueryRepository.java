0a03e5a944e45db19e6fc6c20a0d67a9097cd563
이도하 <victoryddh5@naver.com>
2023-11-09 09:42:56 +0900

    [BE] REFACTOR : 미사용 import 삭제

--- memberService/src/main/java/com/itda/memberservice/notice/repository/NoticeQueryRepository.java
+++ memberService/src/main/java/com/itda/memberservice/notice/repository/NoticeQueryRepository.java
@@ -4,7 +4,6 @@ import com.itda.memberservice.notice.dto.response.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface NoticeQueryRepository {
 
