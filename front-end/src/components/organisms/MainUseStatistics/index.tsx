2c727bb825706b04835ea0654a2f9fa57f4e53d2
문지은 <mjieun0956@gmail.com>
2023-11-14 10:22:12 +0900

    [FE] REFACTOR : 코드 리팩토링

--- front-end/src/components/organisms/MainUseStatistics/index.tsx
+++ front-end/src/components/organisms/MainUseStatistics/index.tsx
@@ -11,6 +11,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 import useMyApi from '@/hooks/useMyApi';
+import useUrl from '@/hooks/useUrl';
 import CategoryUsage from '../statistics/CategoryUsage';
 
 function MainUseStatistics() {
@@ -20,6 +21,7 @@ function MainUseStatistics() {
   const defaultTeamName = userInfo?.teams[0]?.teamName;
   const initialTeam = selectedTeam ?? defaultTeamName;
   const { useCategoryList } = useMyApi(initialTeam);
+  const { userStatisticsUrl } = useUrl(initialTeam);
 
   if (useCategoryList === undefined) {
     return null;
@@ -48,7 +50,7 @@ function MainUseStatistics() {
           </svg>
           <div className="samsungLogo">사용량 통계</div>
         </div>
-        <button type="button" className={styles.goDetail} onClick={() => router.push('/')}>
+        <button type="button" className={styles.goDetail} onClick={() => router.push(userStatisticsUrl)}>
           상세보기
           <svg
             className="w-3 h-3 pl-2 text-gray-500 dark:text-white"
--- front-end/src/hooks/useUrl.ts
+++ front-end/src/hooks/useUrl.ts
@@ -2,14 +2,15 @@ import useMyApi from './useMyApi';
 import useApi from './useApi';
 
 function useUrl(teamName: string) {
-  const { firstUseCategoryId } = useMyApi(teamName);
+  const { firstUseCategoryId, firstProvideCategoryId } = useMyApi(teamName);
   const { firstCategoryId } = useApi();
 
   const userStatisticsUrl = `/statistics/category/use/${firstUseCategoryId}`;
+  const userProvideStatisticsUrl = `/statistics/category/provide/${firstProvideCategoryId}`;
   const adminStatisticsUrl = `/statistics/category/admin/${firstCategoryId}`;
   const categoryUrl = `/category/${firstCategoryId}`;
 
-  return { userStatisticsUrl, adminStatisticsUrl, categoryUrl };
+  return { userStatisticsUrl, adminStatisticsUrl, categoryUrl, userProvideStatisticsUrl };
 }
 
 export default useUrl;
