b46d46f42d602ee192b2f02315361a42deebdae0
이도하 <victoryddh5@naver.com>
2023-11-15 15:59:06 +0900

    [BE] FIX : 에러 코드 수정중 (미완)

--- memberService/src/main/java/com/itda/memberservice/error/ErrorResponseEntity.java
+++ memberService/src/main/java/com/itda/memberservice/error/ErrorResponseEntity.java
@@ -2,16 +2,20 @@ package com.itda.memberservice.error;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 
 @Data
 @Builder
+@Slf4j
 public class ErrorResponseEntity {
     private int status;
     private String code;
     private String message;
 
     public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode errorCode) {
+
+        log.info("{}, {}, {}", errorCode.getHttpStatus().value(), errorCode.name(), errorCode.getMessage());
 
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
