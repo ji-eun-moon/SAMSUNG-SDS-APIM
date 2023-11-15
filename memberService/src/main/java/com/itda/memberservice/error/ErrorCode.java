ea6f8fb44e6402d189f16e9f839587fba2c6051d
이도하 <victoryddh5@naver.com>
2023-11-15 16:34:27 +0900

    [BE] FEAT : 에러 코드 변경

--- memberService/src/main/java/com/itda/memberservice/error/ErrorCode.java
+++ memberService/src/main/java/com/itda/memberservice/error/ErrorCode.java
@@ -16,7 +16,10 @@ public enum ErrorCode {
     // notice
     NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 쪽지를 찾을 수 없습니다."),
     NOTICE_USER_INVALID(HttpStatus.UNAUTHORIZED, "해당 쪽지에 권한이 없습니다."),
-    NOTICE_BAD_REQUEST(HttpStatus.BAD_REQUEST, "올바른 형식의 쪽지가 아닙니다.");
+    NOTICE_BAD_REQUEST(HttpStatus.BAD_REQUEST, "올바른 형식의 쪽지가 아닙니다."),
+
+    // team
+    TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 팀을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
