ecf3b89e766e323af0ee1e33011a0fe752ccebe4
문지은 <mjieun0956@gmail.com>
2023-11-17 08:54:27 +0900

    [FE] DESIGN : 알림창 색상 변경

--- front-end/src/components/organisms/NewNotice/index.tsx
+++ front-end/src/components/organisms/NewNotice/index.tsx
@@ -259,13 +259,13 @@ function NewNotice() {
         <div
           aria-hidden
           id="toast-notification"
-          className="w-full p-4 text-gray-900 bg-white rounded-lg shadow border-2 border-yellow-500"
+          className="w-full p-4 text-gray-900 bg-white rounded-lg shadow border-2 border-green-500"
           role="alert"
         >
           <div className="flex items-center mb-3 gap-2 justify-between">
             <div className="flex gap-2">
               <svg
-                className="w-6 h-6 text-yellow-500 dark:text-white"
+                className="w-6 h-6 text-green-500 dark:text-white"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -273,7 +273,7 @@ function NewNotice() {
               >
                 <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
               </svg>
-              <p className="mb-1 text-base font-semibold text-yellow-500 dark:text-white">
+              <p className="mb-1 text-base font-semibold text-green-500 dark:text-white">
                 {newApply.applyName} 신청 알림
               </p>
             </div>
